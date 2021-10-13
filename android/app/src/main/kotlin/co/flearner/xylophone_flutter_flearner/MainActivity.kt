73d1830674ab69ed3dd912d6c5ec39d836203899
EUNJEONGSONG <hellogine12@gmail.com>
2021-10-14 00:15:06 +0900

    add englich-words package

---
+++ android/app/src/main/kotlin/co/flearner/xylophone_flutter_flearner/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.flearner.xylophone_flutter_flearner
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(const XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(child: Text(nouns.last)),
+        ),
+      ),
+    );
+  }
+}
