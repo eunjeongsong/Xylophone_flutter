187e6fd303e78eaacf8e2875ebd73903de070704
EUNJEONGSONG <hellogine12@gmail.com>
2021-10-14 23:03:17 +0900

    Add sound TextButtons

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(const XylophoneApp());
 
@@ -11,7 +12,86 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child: Text(nouns.last)),
+          child: Center(
+            child: Column(children: <Widget>[
+              Expanded(
+                  child: TextButton(
+                onPressed: () {
+                  AudioCache audioCache = AudioCache();
+                  audioCache.play('note1.wav');
+                },
+                child: Container(
+                  color: Colors.red,
+                ),
+              )),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    AudioCache audioCache = AudioCache();
+                    audioCache.play('note2.wav');
+                  },
+                  child: Container(
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    AudioCache audioCache = AudioCache();
+                    audioCache.play('note3.wav');
+                  },
+                  child: Container(
+                    color: Colors.yellow,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    AudioCache audioCache = AudioCache();
+                    audioCache.play('note4.wav');
+                  },
+                  child: Container(
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    AudioCache audioCache = AudioCache();
+                    audioCache.play('note5.wav');
+                  },
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    AudioCache audioCache = AudioCache();
+                    audioCache.play('note6.wav');
+                  },
+                  child: Container(
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(
+                  onPressed: () {
+                    AudioCache audioCache = AudioCache();
+                    audioCache.play('note7.wav');
+                  },
+                  child: Container(
+                    color: Colors.purple,
+                  ),
+                ),
+              ),
+            ]),
+          ),
         ),
       ),
     );
